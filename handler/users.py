3dd33d45a4c2c00c724b90a4f40a483cbf55edb7
DavidTatis <datapo14@gmail.com>
2020-03-31 01:45:43 -0400

    CRUD Users hardcoded

---
+++ handler/users.py
@@ -0,0 +1,24 @@
+from flask import jsonify
+#from dao.users import UsersDAO
+
+
+class UserHandler:
+    def getAllUsers(self):
+        users = [
+            {"user_id":1, "name":'Ariel'},
+            {"user_id": 2, "name": 'David'},
+            {"user_id": 3, "name": 'Javier'},
+        ]
+        return jsonify(Users=users), 200
+
+    def getUserById(self, user_id):
+        users = [
+            {"user_id": 1, "name": 'Ariel'}
+        ]
+        return jsonify(Users=users), 200
+
+    def searchUsers(self, args):
+        users = [
+            {"user_id": 1, "name": 'Ariel'}
+        ]
+        return jsonify(Users=users,Args=args), 200
--- routes/user.py
+++ routes/user.py
@@ -1,13 +1,30 @@
-from flask import Blueprint, request
+from flask import Blueprint, request,jsonify
+from handler.users import UserHandler
 users = Blueprint('users', __name__)
 
-@users.route("/", methods=('GET', 'POST'))
-def index_page():
+@users.route('/', methods=['GET', 'POST'])
+def getAllUsers():
     if request.method == 'POST':
-        return "This is a  POST burritos"+str(request.json)
+      #insert a user
+      return jsonify(Message="User created."), 200
     else:
-        return "This is a GET burritos"
+        if not request.args:
+            return UserHandler().getAllUsers()
+        else:
+            return UserHandler().searchUsers(request.args)
 
-@users.route("/about")
-def about_page():
-  return "This is a website about burritos"
+
+@users.route('/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def getUserById(user_id):
+    if request.method == 'GET':
+        return UserHandler().getUserById(user_id)
+    elif request.method == 'PUT':
+        #UserHandler().updateUser(user_id,request.form)
+        return jsonify(Message="User update successful"), 200
+    elif request.method == 'DELETE':
+        #UserHandler().deleteUser(user_id)
+        return jsonify(Message="User delete successful"), 200
+    else:
+        return jsonify(Error="Method not allowed."), 405
+
+
