8b821acc1cbed402d8b745d4658d86741099a9d1
Ariel Silva <ariel.silva@upr.edu>
2020-03-31 23:13:16 -0400

    Added batteries, power generators, tools and clothing getters in routes and handler folder. Removed quantity duplicate

--- handler/resources.py
+++ handler/resources.py
@@ -4,41 +4,126 @@ from flask import jsonify
 class ResourcesHandler:
     def getAllResources(self):
         resources = [
-            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani', "quantity": 100, "price": 1.00, "lat": 18.0111, "long": 66.6141},
-            {"resource_id": 2, "quantity": 23, "description": 'Bolsa de hielo 5 libras', "brand": 'La Hielera Inc', "quantity": 10, "price": 5.00, "lat": 18.0121, "long": 65.6181},
-            {"resource_id": 3, "quantity": 5, "description": 'Salchichas Carmela', "brand": 'Carmela', "quantity": 500, "price": 0.59, "lat": 18.0111, "long": 64.9876},
+            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani', "price": 1.00, "lat": 18.0111, "long": 66.6141},
+            {"resource_id": 2, "quantity": 23, "description": 'Bolsa de hielo 5 libras', "brand": 'La Hielera Inc', "price": 5.00, "lat": 18.0121, "long": 65.6181},
+            {"resource_id": 3, "quantity": 5, "description": 'Salchichas Carmela', "brand": 'Carmela',  "price": 0.59, "lat": 18.0111, "long": 64.9876},
         ]
         return jsonify(resources=resources), 200
 
     def getResourceById(self, resource_id):
         resources = [
-            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani', "quantity": 100, "price": 1.00, "lat": 18.0111, "long": 66.6141}
+            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani', "price": 1.00, "lat": 18.0111, "long": 66.6141}
         ]
         return jsonify(Resources=resources), 200
 
     def searchResources(self, args):
         resources = [
-            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani', "quantity": 100, "price": 1.00, "lat": 18.0111, "long": 66.6141}
+            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani', "price": 1.00, "lat": 18.0111, "long": 66.6141}
         ]
         return jsonify(Resources=resources,Args=args), 200
 
     #=============== WATER ===========================
     def getAllWaterResources(self):
         resources = [
-            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani', "quantity": 100, "price": 1.00, "lat": 18.0111, "long": 66.6141, "calories":0, "speciality":"mineral","container":"six pack","ounces_per_bottle":24},
-            {"resource_id": 2, "quantity": 23, "description": 'Bolsa de hielo 5 libras', "brand": 'La Hielera Inc', "quantity": 10, "price": 5.00, "lat": 18.0121, "long": 65.6181, "calories":0,"speciality":"mineral","container":"six pack","ounces_per_bottle":24},
-            {"resource_id": 3, "quantity": 5, "description": 'Salchichas Carmela', "brand": 'Carmela', "quantity": 500, "price": 0.59, "lat": 18.0111, "long": 64.9876,"calories":0,"speciality":"mineral","container":"six pack","ounces_per_bottle":24},
+            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani',  "price": 1.00, "lat": 18.0111, "long": 66.6141, "calories":0, "speciality":"mineral","container":"six pack","ounces_per_bottle":24},
+            {"resource_id": 2, "quantity": 23, "description": 'Bolsa de hielo 5 libras', "brand": 'La Hielera Inc',  "price": 5.00, "lat": 18.0121, "long": 65.6181, "calories":0,"speciality":"mineral","container":"six pack","ounces_per_bottle":24},
+            {"resource_id": 3, "quantity": 5, "description": 'Salchichas Carmela', "brand": 'Carmela',  "price": 0.59, "lat": 18.0111, "long": 64.9876,"calories":0,"speciality":"mineral","container":"six pack","ounces_per_bottle":24},
         ]
         return jsonify(resources=resources), 200
 
     def getWaterResourceById(self, resource_id):
         resources = [
-            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani', "quantity": 100, "price": 1.00, "lat": 18.0111, "long": 66.6141, "calories":0, "speciality":"mineral","container":"six pack","ounces_per_bottle":24}
+            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani',  "price": 1.00, "lat": 18.0111, "long": 66.6141, "calories":0, "speciality":"mineral","container":"six pack","ounces_per_bottle":24}
         ]
         return jsonify(Resources=resources), 200
 
     def searchWaterResources(self, args):
         resources = [
-            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani', "quantity": 100, "price": 1.00, "lat": 18.0111, "long": 66.6141, "calories":0, "speciality":"mineral","container":"six pack","ounces_per_bottle":24}
+            {"resource_id":1, "quantity": 2, "description": 'Botellas de agua 13oz Dasani', "brand": 'Dasani',  "price": 1.00, "lat": 18.0111, "long": 66.6141, "calories":0, "speciality":"mineral","container":"six pack","ounces_per_bottle":24}
         ]
         return jsonify(Resources=resources,Args=args), 200
+    # =============== WATER ===========================
+    # =============== BATTERIES ===========================
+    def getAllBatteryResources(self):
+        resources = [
+            {"resource_id":1, "quantity": 20, "description": 'Baterias AAA', "brand": 'Duracell',  "price": 2.00, "lat": 18.0111, "long": 66.6141, "disposability":'true' , "household_type":'Lithium'},
+            {"resource_id": 2, "quantity": 123, "description": 'Baterias AA', "brand": 'Duracell',  "price": 6.00, "lat": 18.0121, "long": 65.6181, "disposability": 'true' ,"household_type":'Alkaline'},
+            {"resource_id": 3, "quantity": 500, "description": 'Baterias DD', "brand": 'Duracell',  "price": 9.59, "lat": 18.0111, "long": 64.9876,"disposability": 'false' ,"household_type":'Lithium'},
+        ]
+        return jsonify(resources=resources), 200
+
+    def getBatteryResourceById(self, resource_id):
+        resources = [
+            {"resource_id":1, "quantity": 20, "description": 'Baterias AAA', "brand": 'Duracell',  "price": 2.00, "lat": 18.0111, "long": 66.6141, "disposability":'true' , "household_type":'Lithium'},
+        ]
+        return jsonify(Resources=resources), 200
+
+    def searchBatteryResources(self, args):
+        resources = [
+            {"resource_id":1, "quantity": 20, "description": 'Baterias AAA', "brand": 'Duracell',  "price": 2.00, "lat": 18.0111, "long": 66.6141, "disposability":'true' , "household_type":'Lithium'},
+        ]
+        return jsonify(Resources=resources,Args=args), 200
+    # =============== BATTERIES ===========================
+    # =============== POWER GENERATOR ===========================
+    def getAllPowerGeneratorResources(self):
+        resources = [
+            {"resource_id":1, "quantity": 24, "description": 'Inverter Generator', "brand": 'Honda',  "price": 2250.00, "lat": 18.0111, "long": 66.6141, "generator_fuel_type": 'Diesel' , "type": 'Inverter', "wattage": '8000w', "color": 'Red'},
+            {"resource_id": 2, "quantity": 23, "description": 'Portable Generator', "brand": 'Duromax',  "price": 300.00, "lat": 18.0121, "long": 65.6181, "generator_fuel_type": 'Gasoline' ,"type": 'Portable', "wattage": '4000w', "color": 'Blue'},
+            {"resource_id": 3, "quantity": 50, "description": 'Portable Generator', "brand": 'Sportsman',  "price": 900.99, "lat": 18.0111, "long": 64.9876,"generator_fuel_type": 'Gasoline' ,"type": 'Portable', "wattage": '3000w', "color": 'Yellow'},
+        ]
+        return jsonify(resources=resources), 200
+
+    def getPowerGeneratorResourceById(self, resource_id):
+        resources = [
+            {"resource_id":1, "quantity": 24, "description": 'Inverter Generator', "brand": 'Honda',  "price": 2250.00, "lat": 18.0111, "long": 66.6141, "generator_fuel_type": 'Diesel' , "type": 'Inverter', "wattage": '8000w', "color": 'Red'},
+        ]
+        return jsonify(Resources=resources), 200
+
+    def searchPowerGeneratorResources(self, args):
+        resources = [
+            {"resource_id":1, "quantity": 24, "description": 'Inverter Generator', "brand": 'Honda',  "price": 2250.00, "lat": 18.0111, "long": 66.6141, "generator_fuel_type": 'Diesel' , "type": 'Inverter', "wattage": '8000w', "color": 'Red'},
+        ]
+        return jsonify(Resources=resources,Args=args), 200
+    # =============== POWER GENERATOR ===========================
+    # =============== TOOLS ===========================
+    def getAllToolResources(self):
+        resources = [
+            {"resource_id":1, "quantity": 2000, "description": 'Hammer 24', "brand": 'Home Depot',  "price": 2.00, "lat": 18.0111, "long": 66.6141, "material":'Aluminum' , "field":'General'},
+            {"resource_id": 2, "quantity": 123, "description": 'Saw 3 Feetlong', "brand": 'Home Depot',  "price": 16.00, "lat": 18.0121, "long": 65.6181, "material": 'Steel' ,"field":'Carpenter'},
+            {"resource_id": 3, "quantity": 50000, "description": 'Wood Plank 4 feet long', "brand": 'Home Depot',  "price": 4.00, "lat": 18.0111, "long": 64.9876, "material": 'Wood' , "field": 'Carpenter'},
+        ]
+        return jsonify(resources=resources), 200
+
+    def getToolResourceById(self, resource_id):
+        resources = [
+            {"resource_id":1, "quantity": 2000, "description": 'Hammer 24', "brand": 'Home Depot',  "price": 2.00, "lat": 18.0111, "long": 66.6141, "material":'Aluminum' , "field":'General'},
+        ]
+        return jsonify(Resources=resources), 200
+
+    def searchToolResources(self, args):
+        resources = [
+            {"resource_id":1, "quantity": 2000, "description": 'Hammer 24', "brand": 'Home Depot',  "price": 2.00, "lat": 18.0111, "long": 66.6141, "material":'Aluminum' , "field":'General'},
+        ]
+        return jsonify(Resources=resources,Args=args), 200
+    # =============== TOOLS ===========================
+    # =============== CLOTHING ===========================
+    def getAllClothingResources(self):
+        resources = [
+            {"resource_id":1, "quantity": 500, "description": 'Hammer 24', "brand": 'Home Depot',  "price": 2.00, "lat": 18.0111, "long": 66.6141, "Size":'Medium' , "Gender":'Male', "color": 'Red', "material": 'Cotton'},
+            {"resource_id": 2, "quantity": 123, "description": 'Saw 3 Feetlong', "brand": 'Home Depot',  "price": 16.00, "lat": 18.0121, "long": 65.6181, "Size":'Small' , "Gender":'Male', "color": 'Red', "material": 'Cotton'},
+            {"resource_id": 3, "quantity": 50000, "description": 'Wood Plank 4 feet long', "brand": 'Home Depot',  "price": 4.00, "lat": 18.0111, "long": 64.9876, "Size":'Large' , "Gender":'Male', "color": 'Blue', "material": 'Cotton'},
+        ]
+        return jsonify(resources=resources), 200
+
+    def getClothingResourceById(self, resource_id):
+        resources = [
+            {"resource_id":1, "quantity": 500, "description": 'Hammer 24', "brand": 'Home Depot',  "price": 2.00, "lat": 18.0111, "long": 66.6141, "Size":'Medium' , "Gender":'Male', "color": 'Red', "material": 'Cotton'},
+        ]
+        return jsonify(Resources=resources), 200
+
+    def searchClothingResources(self, args):
+        resources = [
+            {"resource_id":1, "quantity": 500, "description": 'Hammer 24', "brand": 'Home Depot',  "price": 2.00, "lat": 18.0111, "long": 66.6141, "Size":'Medium' , "Gender":'Male', "color": 'Red', "material": 'Cotton'},
+        ]
+        return jsonify(Resources=resources,Args=args), 200
+    # =============== CLOTHING ===========================
--- routes/resource.py
+++ routes/resource.py
@@ -52,3 +52,110 @@ def getWaterResourceById(resource_id):
         return jsonify(Message="Water Resource delete successful"), 200
     else:
         return jsonify(Error="Method not allowed."), 405
+
+#====================== WATER ==============================
+#====================== BATTERIES ==============================
+
+@resources.route('/battery/', methods=['GET', 'POST'])
+def getAllBatteryResources():
+    if request.method == 'POST':
+      return jsonify(Message="Battery Resource created."), 200
+    else:
+        if not request.args:
+            return ResourcesHandler().getAllBatteryResources()
+        else:
+            return ResourcesHandler().searchBatteryResources(request.args)
+
+
+@resources.route('/battery/<int:resource_id>', methods=['GET', 'PUT', 'DELETE'])
+def getBatteryResourceById(resource_id):
+    if request.method == 'GET':
+        return ResourcesHandler().getBatteryResourceById(resource_id)
+    elif request.method == 'PUT':
+        #ResourcesHandler().updateBatteryResource(resource_id,request.form)
+        return jsonify(Message="Battery Resource update successful"), 200
+    elif request.method == 'DELETE':
+        #ResourcesHandler().deleteBatteryResource(resource_id)
+        return jsonify(Message="Battery Resource delete successful"), 200
+    else:
+        return jsonify(Error="Method not allowed."), 405
+
+#====================== BATTERIES ==============================
+#====================== POWER GENERATORS ==============================
+
+@resources.route('/PowerGenerator/', methods=['GET', 'POST'])
+def getAllPowerGeneratorResources():
+    if request.method == 'POST':
+      return jsonify(Message="Power Generators Resource created."), 200
+    else:
+        if not request.args:
+            return ResourcesHandler().getAllPowerGeneratorResources()
+        else:
+            return ResourcesHandler().searchPowerGeneratorResources(request.args)
+
+
+@resources.route('/PowerGenerator/<int:resource_id>', methods=['GET', 'PUT', 'DELETE'])
+def getPowerGeneratorResourceById(resource_id):
+    if request.method == 'GET':
+        return ResourcesHandler().getPowerGeneratorResourceById(resource_id)
+    elif request.method == 'PUT':
+        #ResourcesHandler().updatePowerGeneratorResource(resource_id,request.form)
+        return jsonify(Message="Power Generator Resource update successful"), 200
+    elif request.method == 'DELETE':
+        #ResourcesHandler().deletePowerGeneratorResource(resource_id)
+        return jsonify(Message="Power Generator Resource delete successful"), 200
+    else:
+        return jsonify(Error="Method not allowed."), 405
+#====================== POWER GENERATORS ==============================
+#====================== TOOLS ==============================
+
+@resources.route('/tool/', methods=['GET', 'POST'])
+def getAllToolResources():
+    if request.method == 'POST':
+      return jsonify(Message="Tool Resource created."), 200
+    else:
+        if not request.args:
+            return ResourcesHandler().getAllToolResources()
+        else:
+            return ResourcesHandler().searchToolResources(request.args)
+
+
+@resources.route('/PowerGenerator/<int:resource_id>', methods=['GET', 'PUT', 'DELETE'])
+def getToolResourceById(resource_id):
+    if request.method == 'GET':
+        return ResourcesHandler().getToolResourceById(resource_id)
+    elif request.method == 'PUT':
+        #ResourcesHandler().updateToolResource(resource_id,request.form)
+        return jsonify(Message="Tool Resource update successful"), 200
+    elif request.method == 'DELETE':
+        #ResourcesHandler().deleteToolResource(resource_id)
+        return jsonify(Message="Tool Resource delete successful"), 200
+    else:
+        return jsonify(Error="Method not allowed."), 405
+#====================== TOOLS ==============================
+#====================== CLOTHING ==============================
+
+@resources.route('/clothing/', methods=['GET', 'POST'])
+def getAllClothingResources():
+    if request.method == 'POST':
+      return jsonify(Message="Clothing Resource created."), 200
+    else:
+        if not request.args:
+            return ResourcesHandler().getAllClothingResources()
+        else:
+            return ResourcesHandler().searchClothingResources(request.args)
+
+
+@resources.route('/clothing/<int:resource_id>', methods=['GET', 'PUT', 'DELETE'])
+def getClothingResourceById(resource_id):
+    if request.method == 'GET':
+        return ResourcesHandler().getClothingResourceById(resource_id)
+    elif request.method == 'PUT':
+        #ResourcesHandler().updateClothingResource(resource_id,request.form)
+        return jsonify(Message="Clothing Resource update successful"), 200
+    elif request.method == 'DELETE':
+        #ResourcesHandler().deleteClothingResource(resource_id)
+        return jsonify(Message="Clothing Resource delete successful"), 200
+    else:
+        return jsonify(Error="Method not allowed."), 405
+#====================== CLOTHING ==============================
