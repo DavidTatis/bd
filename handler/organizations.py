a7b3d1c32d31b367ef19fdaf422ea9f5966cc04a
DavidTatis <datapo14@gmail.com>
2020-05-03 17:36:49 -0400

    connection to db

--- handler/organizations.py
+++ handler/organizations.py
@@ -1,13 +1,10 @@
 from flask import jsonify
-
+from dao.users import UsersDAO
 
 class OrganizationsHandler:
     def getAllOrganizations(self):
-        organizations = [
-            {"organization_id":1, "name": "Google", "email": 'google@gmail.com', "phone": 7877871234, "zipcode": 12458, "address": 'California'},
-            {"organization_id": 2, "name": "Facebook", "email": 'facebook@facebook.com', "phone": 8024551234, "zipcode": 18568, "address": 'California'},
-            {"organization_id": 3, "name": "Amazon Web Services", "email": 'amz@amazon.com', "phone": 9965401234, "zipcode": 98552, "address": 'California'},
-        ]
+        dao = UsersDAO()
+        organizations=dao.getAllUsers()
         return jsonify(Organizations=organizations), 200
 
     def getOrganizationById(self, organization_id):
